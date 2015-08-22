7501cdfa8845eebad2533aa4b2e130354669656b
Alexandra Edelstein <alexandra.edelstein@gmail.com>
2015-08-22 16:02:31 -0400

    layout

---
+++ app/controllers/staticpages_controller.rb
@@ -0,0 +1,13 @@
+class StaticpagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def products
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'staticpages/home'
+
+  get 'staticpages/about'
+
+  get 'staticpages/products'
+
+  get 'staticpages/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root 'staticpages#home'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
